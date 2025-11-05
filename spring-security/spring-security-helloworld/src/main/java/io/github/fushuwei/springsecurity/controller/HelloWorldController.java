d3a729f69e9b867e824fb19d606c0d0c811f87a5
fushuwei <331044761@qq.com>
2025-11-05 23:51:41 +0800

    提交代码

---
+++ spring-security/spring-security-helloworld/src/main/java/io/github/fushuwei/springsecurity/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package io.github.fushuwei.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
