e1ef0d3dbec54e778d310e391ecaa10650abad99
fushuwei <331044761@qq.com>
2025-11-10 00:53:11 +0800

    提交修改

---
+++ spring-security/spring-security-filterchain/src/main/java/io/github/fushuwei/springsecurity/FilterChainApplication.java
@@ -0,0 +1,12 @@
+package io.github.fushuwei.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilterChainApplication.class, args);
+    }
+}
