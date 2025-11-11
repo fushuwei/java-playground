b9469810bbc25890f7274fdb563bde92ff27fe61
fushuwei <331044761@qq.com>
2025-11-11 23:05:27 +0800

    修改mybatis-plus配置

--- spring-security/spring-security-registerlogin/src/main/java/io/github/fushuwei/springsecurity/RegisterLoginApplication.java
+++ spring-security/spring-security-registerlogin/src/main/java/io/github/fushuwei/springsecurity/RegisterLoginApplication.java
@@ -1,9 +1,11 @@
 package io.github.fushuwei.springsecurity;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("io.github.fushuwei.springsecurity.mapper")
 public class RegisterLoginApplication {
 
     public static void main(String[] args) {
