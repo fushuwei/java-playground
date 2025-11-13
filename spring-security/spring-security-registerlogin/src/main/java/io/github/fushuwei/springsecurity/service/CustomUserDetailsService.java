d987e4a2591c05854ee4acc9cf4767dc09a28d3a
fushuwei <331044761@qq.com>
2025-11-14 01:28:03 +0800

    掌握spring security自动升级密码办法，比如将不安全的{noop}自动转成{bcrypt}

--- spring-security/spring-security-registerlogin/src/main/java/io/github/fushuwei/springsecurity/service/CustomUserDetailsService.java
+++ spring-security/spring-security-registerlogin/src/main/java/io/github/fushuwei/springsecurity/service/CustomUserDetailsService.java
@@ -5,13 +5,14 @@ import io.github.fushuwei.springsecurity.entity.UserDO;
 import io.github.fushuwei.springsecurity.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsManager {
+public class CustomUserDetailsService implements UserDetailsManager, UserDetailsPasswordService {
 
     private final UserMapper userMapper;
 
@@ -54,5 +55,22 @@ public class CustomUserDetailsService implements UserDetailsManager {
     @Override
     public boolean userExists(String username) {
         return false;
+    }
+
+    /**
+     * updatePassword 方法不是由你的应用代码显式调用的。它是由 Spring Security 框架在特定时机自动调用的，
+     * 它的主要目的是为了自动升级用户密码的编码方式，例如从明文或不安全的旧哈希算法（如 MD5）升级到更安全的算法（如 bcrypt），
+     * 当用户成功登录认证后，如果配置了密码升级策略（如当前方法便是配置了升级策略），Spring Security 会自动调用该方法。
+     * <p>
+     * data.sql 脚本中初始化了一个 test 用户，密码是 {noop} 明文存储，使用该用户登录看看数据库中密码字段值有什么变化
+     *
+     * @param newPassword 由 Spring Security 框架自动生成安全加密算法对应的密码，不需要手动进行生成密文
+     */
+    @Override
+    public UserDetails updatePassword(UserDetails user, String newPassword) {
+        userMapper.update(Wrappers.<UserDO>lambdaUpdate()
+            .eq(UserDO::getUsername, user.getUsername())
+            .set(UserDO::getPassword, newPassword));
+        return user;
     }
 }
