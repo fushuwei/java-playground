2012d57da712412f195d3aafc8490ca0f52ca88c
fushuwei <331044761@qq.com>
2025-11-19 14:52:35 +0800

    提交修改

--- spring-security/spring-security-authenticationmanager/src/main/java/io/github/fushuwei/springsecurity/config/SecurityConfiguration.java
+++ spring-security/spring-security-authenticationmanager/src/main/java/io/github/fushuwei/springsecurity/config/SecurityConfiguration.java
@@ -1,22 +1,21 @@
 package io.github.fushuwei.springsecurity.config;
 
+import io.github.fushuwei.springsecurity.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,7 +31,7 @@ public class SecurityConfiguration {
 
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
-        AuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
         return new ProviderManager(provider);
     }
 }
