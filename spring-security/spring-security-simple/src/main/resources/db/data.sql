65a76123046e458f5b57fe70f7e0a4b109f5cc69
fushuwei <331044761@qq.com>
2025-11-30 18:12:27 +0800

    提交修改

--- spring-security/spring-security-simple/src/main/resources/db/data.sql
+++ spring-security/spring-security-simple/src/main/resources/db/data.sql
@@ -3,8 +3,8 @@
 use spring_security;
 
 -- 默认用户 (admin/admin)
-INSERT INTO `user` (`id`, `username`, `nickname`, `realname`, `password`, `age`, `email`) VALUES ('1001', 'admin', '系统管理员', '系统管理员', '{bcrypt}$2a$10$QsR7kxzTPMs/.SOuSf3HCOAdKpEh6lGfQ8tZfCh3.oZLcZ/BQqvIC', 18, 'admin@example.com', now(), now());
+INSERT INTO `user` (`id`, `username`, `nickname`, `realname`, `password`, `age`, `email`, `create_time`, `update_time`) VALUES ('1001', 'admin', '系统管理员', '系统管理员', '{bcrypt}$2a$10$QsR7kxzTPMs/.SOuSf3HCOAdKpEh6lGfQ8tZfCh3.oZLcZ/BQqvIC', 18, 'admin@example.com', now(), now());
 
 -- 该用户用于测试密码加密方式自动升级案例
 DELETE FROM `user` WHERE username = 'test';
-INSERT INTO `user` (`id`, `username`, `nickname`, `realname`, `password`, `age`, `email`) VALUES ('1002', 'test', '测试人员', '测试人员', '{noop}test', 20, 'test@example.com', now(), now());
+INSERT INTO `user` (`id`, `username`, `nickname`, `realname`, `password`, `age`, `email`, `create_time`, `update_time`) VALUES ('1002', 'test', '测试人员', '测试人员', '{noop}test', 20, 'test@example.com', now(), now());
