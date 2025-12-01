bf8640e1ffcf0b6b2667d24bb2daf9fa922f84bb
fushuwei <331044761@qq.com>
2025-12-02 01:06:03 +0800

    提交修改

--- spring-security/spring-security-simple/src/main/java/io/github/fushuwei/security/SpringSecuritySimpleApplication.java
+++ spring-security/spring-security-simple/src/main/java/io/github/fushuwei/security/SpringSecuritySimpleApplication.java
@@ -1,12 +1,15 @@
 package io.github.fushuwei.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringSecuritySimpleApplication {
 
     static void main(String[] args) {
         SpringApplication.run(SpringSecuritySimpleApplication.class, args);
+        log.info("🚀 应用程序启动完毕！");
     }
 }
